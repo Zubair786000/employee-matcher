43d602c1ea7b234c45d1756dea5ca7e9659a3582
Zubair786000 <zubair.s@carwale.com>
2025-04-12 05:33:26 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 your_project/
-├── streamlit_app.py
-│   └── config.toml
+import streamlit as st
+st.title("Employee Matcher App")
+st.write("Welcome to the app!")
 headless = true
 address = "0.0.0.0"
 port = 5000
